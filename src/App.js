ad34ca208e434708322268983a9e1ac7060bba02
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-03 19:11:23 -0600

    Agregar las rutas al proyecto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 
-import Ordenes from './components/paginas/Ordenes';
+import Ordenes from './components/page/Ordenes';
+import Menu from './components/page/Menu';
+import NuevoPlatillo from './components/page/NuevoPlatillo';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={ <Ordenes /> } />
+        <Route path="/menu" element={ <Menu /> } />
+        <Route path="/nuevo-platillo" element={ <NuevoPlatillo /> } />
       </Routes>
     </div>
   );
