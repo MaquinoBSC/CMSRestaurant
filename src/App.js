be936f480f87fd3bdd1455cdab107df8c3f15b4b
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-03 18:34:24 -0600

    Routing en la aplicacion web

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+
+import Ordenes from './components/paginas/Ordenes';
 
 function App() {
   return (
-    <h1 className="text-3xl font-light">Hola Maquino</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={ <Ordenes /> } />
+      </Routes>
+    </div>
   );
 }
+
+
 
 export default App;
